c0f03409ec52a567834236eb7570c1587cdd1902
TailaneBrito <tailane.brito01@gmail.com>
2017-01-31 22:32:27 -0200

    First changed on Profissional and Client classes

---
+++ src/br/com/kmnos/mekup/classCliente/Client.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kmnos.mekup.classCliente;
+
+/**
+ *
+ * @author tailaneb
+ */
+public class Client {
+    private String nomeCompleto;
+    private int cpf;
+    
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    
+}
---
+++ src/br/com/kmnos/mekup/classProfessional/Professional.java
@@ -0,0 +1,19 @@
+
+package br.com.kmnos.mekup.classProfessional;
+
+/**
+ *
+ * @author tailaneb
+ */
+public class Professional {
+    private int cnpj;
+    private String nomeEmpresa;
+    private String nomePerfil;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    
+    private String m = "isso é um test git";
+}
