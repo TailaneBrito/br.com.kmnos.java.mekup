823fd39cde5238cc18b6c3fb7f85b8f983cfbd73
TailaneBrito <tailane.brito01@gmail.com>
2017-02-12 12:02:09 -0200

    Adicionando Icones aos formulários

---
+++ mekup/src/br/com/kmnos/mekup/classes/Professional.java
@@ -0,0 +1,18 @@
+
+package br.com.kmnos.mekup.classes;
+
+/**
+ *
+ * @author tailaneb
+ */
+public class Professional {
+    private int cnpj;
+    private String nomeEmpresa;
+    private String nomePerfil;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    
+ }
